8b1dcb6a4a831c272ca267d1590c0046afd1c2a0
Rayan <rayanghorbani13@gmail.com>
2026-02-27 09:29:28 +0330

    added a class and two prints

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 print("this is a test file in python language")
+
+print("github")
+a = 8
+print(a)
+
+class car:
+
+    "model" == 1404
+    "brand" == ""
+    def __init__(self):
+
+        brand = self.brand
+
