6c58e714963dd4595988dbea0b0148fee9d1b7d3
Rayan <rayanghorbani13@gmail.com>
2026-02-27 10:10:42 +0330

    made some changes by F1

--- test.py
+++ test.py
@@ -9,14 +9,16 @@ class car:
     "model" == 1404
     "brand" == ""
     "color" == ""
+    "Power" == 100
 
-    def __init__(self, brand, color):
+    def __init__(self, brand, color, power):
 
         brand = self.brand
         color = self.color
+        power = self.power
 
 
-C1 = class.car(brand = "BMW" , color = "White Blue")     
+C1 = class.car(brand = "BMW" , color = "White Blue" , power = 250)     
 
 print(C1.brand)
 print("I love GIT HUB")
